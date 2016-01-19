a2ffc050fb0718923f4949bdbcf297fadae76055
acid-gurn <acid.gurn@gmail.com>
2016-01-19 19:09:53 +0000

    added docstrings, removed useless comments, added comments to myself for changes/fixes

--- scorekeeper.py
+++ scorekeeper.py
@@ -11,6 +11,7 @@ starting_points = 0
 
 
 def main_menu():
+    """Displays the menu on start."""
     choice = 0
     print(
         "Welcome to Deven's Yu-Gi-Oh score keeper! Please select an option:"
@@ -33,7 +34,7 @@ def main_menu():
 
 
 def setup():
-    # print("This is the game function.")
+    """Asks the user for player names and starting number of life points."""
     global player1, player2, starting_points, player1_points, player2_points
     player1 = raw_input("What is player 1's name? ")
     if player1 == "":
@@ -49,6 +50,8 @@ def setup():
                     "How many life points would you like to start with? "))
             if type(starting_points) != int:
                 print("That is not a number! Please start again.")
+                # This does not work! It just shows an error and exits which is
+                # not at all what I want from life...pls fix me deven thank u
             else:
                 print(
                     "Player 1 is " + player1 +
@@ -61,6 +64,7 @@ def setup():
 
 
 def begin(choice=""):
+    """Asks the user if they want to start the game or go back to the menu."""
     choice = raw_input(
         "Would you like to begin? Type 'yes' to begin the game or" +
         " type 'no' to return to the main menu: ")
@@ -74,7 +78,7 @@ def begin(choice=""):
 
 
 def player_order(going_first="", choice=0):
-    # print("This is the game function.")
+    """Helps to choose which player is going first."""
     choice = raw_input(
         "Would you like to decide which player goes first or would you like" +
         " me to flip a coin? Please select an option: \n" +
@@ -91,6 +95,7 @@ def player_order(going_first="", choice=0):
 
 
 def coin_flip(coin=0, result=""):
+    """Flips a coin and returns the result."""
     coin = random.random()
     if coin >= 5:
         result = "heads"
@@ -100,6 +105,7 @@ def coin_flip(coin=0, result=""):
 
 
 def choosing_first_player(choice=0):
+    """Allows the player to manually choose who will go first."""
     choice = raw_input(
         "Please choose which player will go first: \n" +
         "1: Player 1 \n" +
@@ -115,6 +121,9 @@ def choosing_first_player(choice=0):
 
 
 def flipping_for_first_player(flipped_coin=""):
+    """Flips a coin to choose who will go first"""
+    # In the future, change this so players can choose heads or tails instead
+    # of automatically assigning heads to player 1.
     flipped_coin = coin_flip()
     if flipped_coin == "heads":
         going_first = player1
@@ -126,6 +135,8 @@ def flipping_for_first_player(flipped_coin=""):
 
 
 def game(choice=0):
+    """Menu that appears when the game has begun."""
+    # This function probably needs a more meaningful name...
     choice = raw_input(
         "What would you like to do? Please select an option: " +
         "\n1: Add/remove life points from " + player1 +
@@ -146,6 +157,7 @@ def game(choice=0):
 
 
 def player1_points(choice=0):
+    """Allows the user to add or remove points from player 1."""
     print(
         "Would you like to add or remove points from " + player1 + "?\n" +
         "Please select an option: \n" +
@@ -163,10 +175,15 @@ def player1_points(choice=0):
 
 
 def about():
+    """Displays information about??? How to use the thing???"""
     print("This is the about function.")
 
 
 def close():
+    """Closes the thing."""
+    # might change to exit bc I prob don't want to close the actual window at
+    # this point but also I don't want to confuse it with exit() or sys.exit()
+    # or whatever it is idk wow such useful comment
     print("This is the close function.")
 
 main_menu()
